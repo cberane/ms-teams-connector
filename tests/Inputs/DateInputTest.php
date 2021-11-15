d4f5ec329150f6a754eaccce965181c441e5442e
C. Berane <cristoph.berane@berane-it.de>
2021-11-15 14:19:47 +0100

    feature(inputs): added some input fields
    - added DateInput and TextInput
    - added some basic tests for json-parsing

---
+++ tests/Inputs/DateInputTest.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Cberane\MsTeamsConnector\Tests\Inputs;
+
+
+use Cberane\MsTeamsConnector\Inputs\DateInput;
+use PHPUnit\Framework\TestCase;
+
+class DateInputTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function empty_input()
+    {
+        $input = new DateInput();
+
+        self::assertEquals('{"@type":"DateInput"}', $input->toJson());
+    }
+
+    /**
+     * @test
+     */
+    public function set_id()
+    {
+        $input = new DateInput('myID');
+
+        self::assertEquals('{"@type":"DateInput","id":"myID"}', $input->toJson());
+    }
+
+    /**
+     * @test
+     */
+    public function set_title()
+    {
+        $input = new DateInput(null, 'myTitle');
+
+        self::assertEquals('{"@type":"DateInput","title":"myTitle"}', $input->toJson());
+    }
+
+    /**
+     * @test
+     */
+    public function set_complete()
+    {
+        $input = new DateInput('id1', 'title1');
+
+        self::assertEquals('{"@type":"DateInput","id":"id1","title":"title1"}', $input->toJson());
+    }
+}
